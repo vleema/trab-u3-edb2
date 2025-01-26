1d971a67f225751ea22f0479e03ed97eb2f1c731
Bianca Medeiros <biancamm0107@gmail.com>
2025-01-26 15:09:03 -0300

    feat: stock file reader

--- src/stock/src/main.rs
+++ src/stock/src/main.rs
@@ -1,26 +1,63 @@
 mod stock;
 
-use std::io;
-
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use btree::btree::core::BTree;
 use stock::Data;
 
+fn read_file(path: &str) -> io::Result<Vec<Data>> {
+    let mut datas = Vec::new();
+
+    let file = File::open(Path::new(path))?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        if let Some((id, name, stock)) = parse_line(&line) {
+            datas.push(Data::new(id, name, stock));
+        }
+    }
+
+    Ok(datas)
+}
+
+fn parse_line(line: &str) -> Option<(usize, String, usize)> {
+
+    let content = line.trim_matches(|c| c == '{' || c == '}');
+    let parts: Vec<&str> = content.split(',').map(|s| s.trim()).collect();
+
+    if parts.len() == 3 {
+  
+        if let (Ok(id), Some(name), Ok(stock)) = (
+            parts[0].parse::<usize>(),
+            parts.get(1).map(|s| s.trim_matches('"').to_string()),
+            parts[2].parse::<usize>(),
+        ) {
+            return Some((id, name, stock));
+        }
+    }
+
+    None
+}
+
 fn main() -> io::Result<()> {
-    let arroz = Data::new(1, "arroz".to_string(), 40);
-    let feijao = Data::new(2, "feijao".to_string(), 53);
-    let macarrao = Data::new(3, "macarrao".to_string(), 60);
-    let alface = Data::new(4, "alface".to_string(), 30);
-    let farofa = Data::new(5, "farofa".to_string(), 70);
-    let carne = Data::new(6, "carne".to_string(), 2);
     let mut tree: BTree<Data, 2> = BTree::new();
 
-    tree.insert(arroz);
-    tree.insert(feijao);
-    tree.insert(macarrao);
-    tree.insert(alface);
-    tree.insert(farofa);
-    tree.insert(carne);
+    let file_path = "dadosB.txt";
+
+    match read_file(file_path) {
+        Ok(dados) => {
+            for data in dados {
+                tree.insert(data);
+            }
+        }
+        Err(erro) => {
+            eprintln!("Erro ao ler o arquivo: {}", erro);
+        }
+    }
 
     tree.generate_graph("out/dot/datas.dot")?;
     Ok(())
 }
+
